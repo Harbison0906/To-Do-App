db025e44dd8bdb313951e2e0fa45e610e5431928
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2021-01-07 21:07:30 -0600

    building up server side; creating folders for routes and database; building out config file

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,22 @@
 import * as express from 'express';
-import apiRouter from './routes';
+import * as morgan from 'morgan';
+import * as passport from 'passport';
+import routes from './routes';
+import cors = require('cors');
+import path = require('path');
+import './middleware/bearerstrategy';
+import './middleware/localstrategy';
+
 
 const app = express();
 
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(passport.initialize())
 app.use(express.static('public'));
-app.use(apiRouter);
+app.use(routes);
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
