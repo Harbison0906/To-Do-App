7cbc0ae5605038982bf93c8bd474cfa802fad9f4
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2021-01-11 20:04:44 -0600

    adding auth routing, register and login routes

---
+++ src/server/routes/auth/login.ts
@@ -0,0 +1,24 @@
+import * as express from 'express';
+import * as passport from 'passport';
+import { createToken } from '../../utils/tokens'
+import { ReqUser } from '../../utils/types'
+
+
+const router = express.Router();
+
+
+router.post('/', passport.authenticate('local'), async (req: ReqUser, res) => {
+  try {
+  let token = await createToken({userid: req.user.id});
+    res.json({
+      token,
+      role: req.user.role,
+      userid: req.user.id
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: 'bad code', error});
+  }
+})
+
+export default router;
